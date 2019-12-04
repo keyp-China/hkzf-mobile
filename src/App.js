bb8a4136f8d28ccd7f40f00c746d9ef377807813
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-12-04 16:36:37 +0800

    antd-mobile引入使用
    antd-mobile引入使用

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// 导入antd-mobile组件
+import { Button } from 'antd-mobile'
+
 export default class App extends React.Component {
     render() {
         return <div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// 全局导入antd-mobile样式
+import 'antd-mobile/dist/antd-mobile.css'
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById("root"))
