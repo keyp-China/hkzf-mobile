ac99a85c1750685dd5a1bb97ee12aca06fb909bf
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-12-04 17:03:58 +0800

    引入react-router-dom
    配置路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-// 导入antd-mobile组件
+// 导入 antd-mobile组件
 import { Button } from 'antd-mobile'
+// 导入 react路由
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Home from "./pages/Home"
+import Citylist from "./pages/Citylist"
 
 export default class App extends React.Component {
     render() {
-        return <div>
-            我是根组件
-        </div>
+        return <Router>
+            <div>
+                <Route path="/home" component={Home}></Route>
+                <Route path="/citylist" component={Citylist}></Route>
+            </div>
+        </Router>
     }
 }
---
+++ src/pages/Citylist/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default class Citylist extends React.Component {
+    render() {
+        return <div>
+            我是Citylist组件
+        </div>
+    }
+}
