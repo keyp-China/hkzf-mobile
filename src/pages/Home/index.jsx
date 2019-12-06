4b70a77a03891a04315cde3d6dae345ed77d0704
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-12-06 17:24:14 +0800

    循环生成tabbar
    循环生成tabbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,11 +12,40 @@ import Profile from '../Profile'
 // 导入home样式
 import './home.css'
 
+/* tabbarItems数据 */
+const tabbarItems = [
+    { title: "首页", icon: "icon-ind", path: "/home/index" },
+    { title: "找房", icon: "icon-findHouse", path: "/home/houselist" },
+    { title: "资讯", icon: "icon-infom", path: "/home/news" },
+    { title: "我的", icon: "icon-my", path: "/home/profile" }
+]
+
 export default class Home extends React.Component {
     // 官网state
     state = {
         selectedTab: '/home/index',
         hidden: false
+    }
+
+    /* tabbarItems渲染 */
+    renderTabbar() {
+        return tabbarItems.map(item => {
+            return <TabBar.Item
+                title={item.title}
+                key={item.path}
+                icon={<i className={`iconfont ${item.icon}`}></i>} //图标
+                selectedIcon={<i className={`iconfont ${item.icon}`}></i>} //选中图标
+                selected={this.state.selectedTab === item.path}
+                onPress={() => { // 点击
+                    this.setState({
+                        selectedTab: item.path,
+                    });
+                    // 路由跳转
+                    this.props.history.push(item.path)
+                }}
+            >
+            </TabBar.Item>
+        })
     }
 
     render() {
@@ -32,65 +61,8 @@ export default class Home extends React.Component {
                 hidden={this.state.hidden} //是否隐藏
                 noRenderContent={true} //true 代表不渲染内容
             >
-                <TabBar.Item
-                    title="首页"
-                    key="index"
-                    icon={<i className="iconfont icon-ind"></i>} //图标
-                    selectedIcon={<i className="iconfont icon-ind"></i>} //选中图标
-                    selected={this.state.selectedTab === '/home/index'}
-                    onPress={() => { // 点击
-                        this.setState({
-                            selectedTab: '/home/index',
-                        });
-                        // 路由跳转
-                        this.props.history.push("/home/index")
-                    }}
-                    data-seed="logId"
-                >
-                </TabBar.Item>
-                <TabBar.Item
-                    icon={<i className="iconfont icon-findHouse"></i>}
-                    selectedIcon={<i className="iconfont icon-findHouse"></i>}
-                    title="找房"
-                    key="findHouse"
-                    selected={this.state.selectedTab === '/home/houselist'}
-                    onPress={() => {
-                        this.setState({
-                            selectedTab: '/home/houselist',
-                        });
-                        this.props.history.push("/home/houselist")
-                    }}
-                    data-seed="logId1"
-                >
-                </TabBar.Item>
-                <TabBar.Item
-                    icon={<i className="iconfont icon-infom"></i>}
-                    selectedIcon={<i className="iconfont icon-infom"></i>}
-                    title="资讯"
-                    key="infom"
-                    selected={this.state.selectedTab === '/home/news'}
-                    onPress={() => {
-                        this.setState({
-                            selectedTab: '/home/news',
-                        });
-                        this.props.history.push("/home/news")
-                    }}
-                >
-                </TabBar.Item>
-                <TabBar.Item
-                    icon={<i className="iconfont icon-my"></i>}
-                    selectedIcon={<i className="iconfont icon-my"></i>}
-                    title="我的"
-                    key="my"
-                    selected={this.state.selectedTab === '/home/profile'}
-                    onPress={() => {
-                        this.setState({
-                            selectedTab: '/home/profile',
-                        });
-                        this.props.history.push("/home/profile")
-                    }}
-                >
-                </TabBar.Item>
+                {/* 循环生成tabbar */}
+                {this.renderTabbar()}
             </TabBar>
         </div>
     }
