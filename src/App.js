90cb756e798668903a71d03414465f19038eb5d6
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-12-06 15:30:57 +0800

    /重定向到/home/index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // 导入 react路由
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 import Home from "./pages/Home"
 import Citylist from "./pages/Citylist"
@@ -10,6 +10,14 @@ export default class App extends React.Component {
     render() {
         return <Router>
             <div className="app">
+                {/* 重定向  exact--精准匹配 */}
+                <Route
+                    exact
+                    path="/"
+                    render={() => {
+                        return <Redirect to="/home/index"></Redirect>
+                    }}>
+                </Route>
                 <Route path="/home" component={Home}></Route>
                 <Route path="/citylist" component={Citylist}></Route>
             </div>
