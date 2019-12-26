7cd677773261fabc046a77667999ffab074271a0
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-12-26 13:40:50 +0800

    地图找房-根据城市名转成经纬度显示地图

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import NavHeader from '../../components/NavHeader'
+import { getCurrentCity } from '../../utils/index'
 import "./map.scss"
 
 //react使用百度地图不能直接使用BMap 需要使用window.BMap
@@ -11,10 +12,25 @@ export default class Map extends React.Component {
     }
 
     /* 初始化百度地图 */
-    initMap() {
+    async initMap() {
+        // 获取当前城市信息
+        let currentCity = await getCurrentCity()
+        console.log(currentCity);
+        // 初始化一个地图
         var map = new BMap.Map("container");
-        var point = new BMap.Point(116.404, 39.915);  //设置中心点坐标 经纬度
-        map.centerAndZoom(point, 11);   //显示地图并缩放 值越大越详细
+
+        // 创建地址解析器实例
+        var myGeo = new BMap.Geocoder()
+        // 将地址解析结果显示在地图上，并调整地图视野    
+        myGeo.getPoint(currentCity.label, function (point) {
+            if (point) {
+                map.centerAndZoom(point, 11);
+                // map.addOverlay(new BMap.Marker(point));
+            }
+        }, currentCity.label);
+
+        /* var point = new BMap.Point(116.404, 39.915);  //设置中心点坐标 经纬度
+        map.centerAndZoom(point, 11);   //显示地图并缩放 值越大越详细 */
     }
     render() {
         return <div className="map">
